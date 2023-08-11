4e2da1b5b72cfa6084ba7e278ae221d6c4d49083
raselsharif <raselsharif9@gmail.com>
2023-08-12 00:09:14 +0600

    style in js

--- js/dom.js
+++ js/dom.js
@@ -1,28 +1,34 @@
 console.log(document);
 
 const liCollection = document.getElementsByTagName('li');
-for(const li of liCollection){
+for (const li of liCollection) {
     // console.log(li.innerText);
 }
 
 const headings = document.getElementsByTagName('h1');
-for(const head of headings){
+for (const head of headings) {
     // console.log(head.innerHTML);
 }
 // console.log(liCollection);
 
 const listItem = document.getElementsByClassName('list-item');
-for(const list of listItem){
+for (const list of listItem) {
     // console.log(list.innerText);
 }
 
 const headID = document.getElementById('heading-two');
 // console.log(headID.innerText="Heading Changed by JS");
+headID.style.color = 'red';
 
 const list = document.querySelector('.list li');
-console.log(list.innerHTML);
+// console.log(list.innerHTML);
 
 const listAll = document.querySelectorAll('.list li');
-for(const listAlls of listAll){
-    console.log(listAlls.innerHTML);
+for (const listAlls of listAll) {
+    // console.log(listAlls.innerHTML);
 }
+
+const listClass = document.querySelectorAll('.list');
+// listClass.style.color = 'red';
+
+// console.log(listClass);
---
+++ js/style.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll("section");
+for (const section of sections) {
+    section.style.border = "1px solid red";
+    section.style.backgroundColor = "teal"
+    section.style.padding = "10px"
+    section.style.marginBottom = "10px"
+    console.log(section);
+}
+
+const heading = document.getElementsByClassName("headingTwo");
+for (const headings of heading) {
+    headings.style.backgroundColor = "red"
+    headings.style.color = "white";
+    headings.style.padding = "15px";
+}
+// const headingId = document.getElementById("headingTwo");
+
+// headingId.style.backgroundColor = 'red';
+// headingId.style.color = "white";
+// headingId.style.padding = "15px";
