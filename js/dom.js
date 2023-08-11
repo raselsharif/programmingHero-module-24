4c7baf3fc5556558505661b819c4412c0f19f1ba
raselsharif <raselsharif9@gmail.com>
2023-08-11 20:05:34 +0600

    getElementsByTagName

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,12 @@
 console.log(document);
+
+const liCollection = document.getElementsByTagName('li');
+for(const li of liCollection){
+    console.log(li.innerText);
+}
+
+const headings = document.getElementsByTagName('h1');
+for(const head of headings){
+    console.log(head.innerHTML);
+}
+// console.log(liCollection);
