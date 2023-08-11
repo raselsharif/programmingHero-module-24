890239905d0e9325db6fa85af440b11143cde062
raselsharif <raselsharif9@gmail.com>
2023-08-11 20:41:07 +0600

    getElementByClass VS getElementById

--- js/dom.js
+++ js/dom.js
@@ -2,11 +2,19 @@ console.log(document);
 
 const liCollection = document.getElementsByTagName('li');
 for(const li of liCollection){
-    console.log(li.innerText);
+    // console.log(li.innerText);
 }
 
 const headings = document.getElementsByTagName('h1');
 for(const head of headings){
-    console.log(head.innerHTML);
+    // console.log(head.innerHTML);
 }
 // console.log(liCollection);
+
+const listItem = document.getElementsByClassName('list-item');
+for(const list of listItem){
+    console.log(list.innerText);
+}
+
+const headID = document.getElementById('heading-two');
+console.log(headID.innerText="Heading Changed by JS");
